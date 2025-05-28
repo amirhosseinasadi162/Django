cfb209ab04611c4e65d09d4fb4a60ce738e7d0c5
unknown <amirhosseinasadi162@gmail.com>
2025-05-28 18:52:51 +0330

    write function add_task in views and create add_task.html

--- task/views.py
+++ task/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
 def task_list(request):
     tasks = Task.objects.all().order_by('-created_at')
     return render(request,'task/task_list.html',{'task':tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+
+    return render(request,'task/add_task.html',{'form':form})
+    
