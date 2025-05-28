41bc396c177c38ff5eab5be8da34f5f5dee74784
unknown <amirhosseinasadi162@gmail.com>
2025-05-28 19:59:33 +0330

    add function edite_task and delete_task and create templates and edit urls

--- task/views.py
+++ task/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
+
 from .models import Task
+
 from .forms import TaskForm
 
 # Create your views here.
 
+# show tasks
 def task_list(request):
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks = Task.objects.all()
     return render(request,'task/task_list.html',{'task':tasks})
 
+# add task
 def add_task(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -18,4 +22,23 @@ def add_task(request):
         form = TaskForm()
 
     return render(request,'task/add_task.html',{'form':form})
-    
+
+#edit task 
+def edit_task(request,task_id):
+    task = get_object_or_404(Task,id = task_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance = task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance = task)
+    return render(request,'task/edit_task.html',{'form':form,'task':task})
+
+#delete task
+def delete_task(request,task_id):
+    task = get_object_or_404(Task,id = task_id)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request,'task/delete_task.html',{'task':task})
